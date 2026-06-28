386c5b56aad60e88ade7d46a2f4ccb495755b0d7
Bhumi Gupta <bhumigupta173@gmail.com>
2026-06-28 22:45:03 +0530

    added Point solution

---
+++ Week_4/Day_6/Question 3 /Point.java
@@ -0,0 +1,34 @@
+public class Point {
+
+    private int x;
+    private int y;
+
+    // Default constructor
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    // Parameterized constructor
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void display() {
+        System.out.println("Point = (" + x + "," + y + ")");
+    }
+}
